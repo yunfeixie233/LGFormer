393bd1476ecf4adb6bccca656f319381c69e68e6
yunfeixie <x908717327@gmail.com>
2023-07-06 20:56:40 +0800

    Update dataset config

--- configs/_base_/datasets/ade20k.py
+++ configs/_base_/datasets/ade20k.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'ADE20KDataset'
-data_root = '/root/autodl-tmp/mmsegmentation/data/ade/ADEChallengeData2016'
+data_root = 'data/ade/ADEChallengeData2016'
 crop_size = (512, 512)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
