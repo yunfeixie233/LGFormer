db90bb547aaee0bade60c4f23c51663f3e5ecb5d
yunfeixie <x908717327@gmail.com>
2023-09-28 06:57:31 +0800

    Merge the newest change in model code

--- mmseg/models/decode_heads/SuperformerBottleNeck_ori.py
+++ mmseg/models/decode_heads/SuperformerBottleNeck_ori.py
@@ -876,6 +876,7 @@ class SuperformerBottleNeck_ori(BaseDecodeHead):
         use_compact_loss: bool= False,
         use_patch_embed: bool = False,
         resize_similarity: bool =True,
+        final_iter: int = 2,
         **kwargs
 
     ):
@@ -885,6 +886,7 @@ class SuperformerBottleNeck_ori(BaseDecodeHead):
         out_channels=seg_num_classes,
         in_index=0,
 **kwargs)
+        self.final_iter = final_iter
         self.use_patch_embed  = use_patch_embed
         self.resize_similarity = resize_similarity
         if self.use_patch_embed:
@@ -1231,7 +1233,7 @@ class SuperformerBottleNeck_ori(BaseDecodeHead):
             assert sp_dim is not None
             sp_fn = partial(
                 st.SuperPixelTokenizationCrossAttentionAsymmetry,
-                self.sp_iter,
+                self.sp_iter if i != len(self.sp_features_init_methods) -1  else self.final_iter,
                 sp_head,
                 pixel_dim,
                 pixel_shape,
@@ -1697,6 +1699,10 @@ class SuperformerBottleNeck_ori(BaseDecodeHead):
                     pixel_logits = superpixel_ops.expand_superpixel_features(
                         sp_logits, similarities
                     )
+                else:
+                    raise ValueError()
+
+            return sp_logits, pixel_logits
         elif self.classification_feature == "superpixel_extralayer_similarity": 
             #final info               
             last_stage = self.stages[-1]
