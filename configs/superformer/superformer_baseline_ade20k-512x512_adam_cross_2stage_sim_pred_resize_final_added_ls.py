d89f51af0d14aa26457c959574e202e4141bf069
meijieru <meijieru@gmail.com>
2023-09-28 00:07:22 -0400

    chore: base configs

---
+++ configs/superformer/superformer_baseline_ade20k-512x512_adam_cross_2stage_sim_pred_resize_final_added_ls.py
@@ -0,0 +1,8 @@
+_base_ = [
+    "./superformer_baseline_ade20k-512x512_adam_cross_2stage_sim_pred_resize_final_added.py"
+]
+model = dict(
+    decode_head=dict(
+        ls_init_value=1e-5,
+    ),
+)
