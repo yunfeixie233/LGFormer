bcd00927229cb09d724ee74815682557510a43ed
yunfeixie <x908717327@gmail.com>
2023-10-08 23:39:12 +0000

    update: add gt ls

--- mmseg/GPViT/mmcls/gpvit_dev/models/utils/attentions.py
+++ mmseg/GPViT/mmcls/gpvit_dev/models/utils/attentions.py
@@ -342,12 +342,13 @@ class FullAttnCatBlock(nn.Module):
             q = self.norm_query(query)
             k = q if self.key_is_query else self.norm_key(key)
             v = k if self.value_is_key else self.norm_value(value)
+
             new_x, attn_dict_list = self.attn(q, k, v, att_bias=att_bias,attn_dict_list = attn_dict_list)
             new_x = torch.cat((query, self.drop_path(new_x)),dim=-1)
             new_x = self.proj(new_x)
-            x = self.ffn(self.norm2(new_x), identity=query)
+            x = self.ffn(self.norm2(new_x), identity=query)     
+                       
             return x,attn_dict_list
-
         if self.with_cp:
             return cp.checkpoint(_inner_forward, query, key, value, att_bias)
         else:
