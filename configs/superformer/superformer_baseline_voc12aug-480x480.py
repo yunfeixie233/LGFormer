1a29e7fbcb75f0d324e668f71e3ef0c5ec0de7a9
yunfeixie <x908717327@gmail.com>
2023-10-10 13:01:54 +0800

    chores: fixed pascal config val iter

--- configs/superformer/superformer_baseline_voc12aug-480x480.py
+++ configs/superformer/superformer_baseline_voc12aug-480x480.py
@@ -52,7 +52,7 @@ param_scheduler = [
     )
 ]
 train_cfg = dict(
-    type='IterBasedTrainLoop', max_iters=40000, val_interval=40000)
+    type='IterBasedTrainLoop', max_iters=40000, val_interval=2000)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 default_hooks = dict(
