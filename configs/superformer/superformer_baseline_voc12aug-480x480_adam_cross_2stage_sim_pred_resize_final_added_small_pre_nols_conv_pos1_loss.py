1548aeed35113437d3cf10f5ff9790f4ef65c934
yunfeixie <x908717327@gmail.com>
2023-10-11 18:36:16 +0000

    chores: config of gt with loss to explore position of gt

--- configs/superformer/superformer_baseline_voc12aug-480x480_adam_cross_2stage_sim_pred_resize_final_added_small_pre_nols_conv_pos1_loss.py
+++ configs/superformer/superformer_baseline_voc12aug-480x480_adam_cross_2stage_sim_pred_resize_final_added_small_pre_nols_conv_pos1_loss.py
@@ -6,11 +6,12 @@ model = dict(
     decode_head=dict(
     ls_init_value = 1e-5,
     resize_version = 'v2',
-    use_group_token = 'mix',
+    use_group_token = 'post',
     arch_settings = {
         'embed_dims': 384,
         'patch_size': 8,
         'window_size': 2,
+        'num_layers': 2,
         'num_heads': 6,
         'num_group_heads': 6,
         'num_group_forward_heads': 6,
@@ -18,14 +19,13 @@ model = dict(
         'ffn_ratio': 4.,
         'patch_embed': dict(type='ConvPatchEmbed', num_convs=0),
         'mlpmixer_depth': 1,
-        'group_layers': {0:100,1:100,},
+        'group_layers': {0:100,},
         'drop_path_rate': 0.2,
         'group_projector_methonds':'linear',
         'association_embedding':False,
         'group_token_init_method':'conv',
-        "init_strides":(3,3,),
-        'init_kernel_sizes':(3,3,),
-        'merge_pos':((),(8,9,),()),
+        "init_strides":(3,),
+        'init_kernel_sizes':(3,),
         'ls_init_value':0.,            
     },
     use_gt_loss = True,
@@ -34,5 +34,4 @@ model = dict(
             type='CrossEntropyLoss',loss_name = 'loss_gt',use_sigmoid=False, loss_weight=0.5),
             dict(
             type='CrossEntropyLoss',loss_name = 'loss_sp', use_sigmoid=False, loss_weight=0.5)]
-    
 ))
