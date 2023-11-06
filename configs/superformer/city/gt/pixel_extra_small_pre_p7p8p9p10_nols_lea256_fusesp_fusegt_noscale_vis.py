2d1ae1cdd069627aa46c1a7503dcdc4ad3bfbae3
yunfeixie <x908717327@gmail.com>
2023-11-06 12:37:52 +0000

    update: auto visualize when eval

--- configs/superformer/city/gt/pixel_extra_small_pre_p7p8p9p10_nols_lea256_fusesp_fusegt_noscale_vis.py
+++ configs/superformer/city/gt/pixel_extra_small_pre_p7p8p9p10_nols_lea256_fusesp_fusegt_noscale_vis.py
@@ -6,4 +6,9 @@ model = dict(
     decode_head=dict(
     group_pos = ((),(5,6,7,8,),()),        
     group_qk_scale = 1,
+    vis_gt_eff = True,
+    vis_sp = True,
+    vis_gt = True,  
+    output_dir = '/data2/yunfei/pixel_extra_small_pre_p7p8p9p10_nols_lea256_fusesp_fusegt_noscale_vis'      
+    
 ))
