0dffa4b0ca31268371703aa2b5e616ef9efb2b8c
yunfeixie <x908717327@gmail.com>
2023-11-11 08:25:54 +0000

    update: add compositor training setting for pascal-part

--- configs/_base_/datasets/pascal_voc12_part.py
+++ configs/_base_/datasets/pascal_voc12_part.py
@@ -8,7 +8,7 @@ train_pipeline = [
     dict(
         type='RandomResize',
         scale=(2048, 512),
-        ratio_range=(0.5, 2.0),
+        ratio_range=(0.1, 2.0),
         keep_ratio=True),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
@@ -40,7 +40,7 @@ tta_pipeline = [
         ])
 ]
 train_dataloader = dict(
-    batch_size=2,
+    batch_size=4,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='InfiniteSampler', shuffle=True),
