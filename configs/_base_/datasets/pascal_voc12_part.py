526c4612bfed9c8ba4672a78bf0d3c7557f21805
yunfeixie <x908717327@gmail.com>
2023-11-12 12:04:39 +0000

    fixed:pascal part config

--- configs/_base_/datasets/pascal_voc12_part.py
+++ configs/_base_/datasets/pascal_voc12_part.py
@@ -17,7 +17,7 @@ train_pipeline = [
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='Resize', scale=crop_size, keep_ratio=True),
+    dict(type='Resize', scale=(2048, 512), keep_ratio=True),
     # add loading annotation after ``Resize`` because ground truth
     # does not need to do resize data transform
     dict(type='LoadAnnotations'),
