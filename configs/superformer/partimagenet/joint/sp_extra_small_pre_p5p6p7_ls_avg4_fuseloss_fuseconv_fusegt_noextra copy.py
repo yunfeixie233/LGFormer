c8864e768d912566b2df4babcb7a712113435266
yunfeixie <x908717327@gmail.com>
2023-11-14 14:58:09 +0000

    merge

---
+++ configs/superformer/partimagenet/joint/sp_extra_small_pre_p5p6p7_ls_avg4_fuseloss_fuseconv_fusegt_noextra copy.py
@@ -0,0 +1,10 @@
+_base_ = [
+    './sp_extra_small_pre_p5p6p7_ls_avg4_fuseloss_fuseconv_fusegt.py',
+]
+num_classes = 57
+model = dict(
+    type='EncoderDecoder',
+    decode_head=dict(
+    classification_feature = 'joint_extralayer',       
+    use_gt_extralayer = False,     
+))
