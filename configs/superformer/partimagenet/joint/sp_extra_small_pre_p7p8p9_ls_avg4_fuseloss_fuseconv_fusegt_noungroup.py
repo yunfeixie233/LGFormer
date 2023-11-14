ae7304a3fd671b55fc991cc43401faf493bd65a4
yunfeixie <x908717327@gmail.com>
2023-11-14 13:27:43 +0000

    chores: add config

--- configs/superformer/partimagenet/joint/sp_extra_small_pre_p7p8p9_ls_avg4_fuseloss_fuseconv_fusegt_noungroup.py
+++ configs/superformer/partimagenet/joint/sp_extra_small_pre_p7p8p9_ls_avg4_fuseloss_fuseconv_fusegt_noungroup.py
@@ -1,5 +1,5 @@
 _base_ = [
-    './sp_extra_small_pre_p7p8p9_ls_avg4_fusegt_fuseloss_fuseconv_loss.py',
+    './sp_extra_small_pre_p7p8p9_ls_avg4_fuseloss_fuseconv_fusegt.py',
 ]
 num_classes = 57
 model = dict(
