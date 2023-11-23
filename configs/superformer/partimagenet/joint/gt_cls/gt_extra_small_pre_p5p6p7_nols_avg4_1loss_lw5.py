920c8139f52e53c5290b60778fae6a10db9a5d9a
yunfeixie <x908717327@gmail.com>
2023-11-23 02:50:26 +0000

    chores: add config for joint training

---
+++ configs/superformer/partimagenet/joint/gt_cls/gt_extra_small_pre_p5p6p7_nols_avg4_1loss_lw5.py
@@ -0,0 +1,15 @@
+_base_ = [
+    './gt_extra_small_pre_p9p10p11_nols_avg4_all.py',
+]
+num_classes = 57
+model = dict(
+    type='EncoderDecoder',
+    decode_head=dict(
+    use_final_group_cls = True,
+    group_pos = ((),(3,4,5,),()),                         
+    loss_decode=[
+            dict(
+            type='CrossEntropyLoss',loss_name = 'loss_sp',use_sigmoid=False, loss_weight=0.5,reduction='mean',),
+            dict(
+            type='CrossEntropyLoss',loss_name = 'loss_gt',use_sigmoid=False, loss_weight=0.5,reduction='mean',)],
+))
