5a6cc5d867b49d7d99817bb2c558f161c4c6622f
yunfeixie <x908717327@gmail.com>
2023-11-26 09:13:23 +0000

    chores: update config

---
+++ configs/superformer/partimagenet/part/mask_extra_small_pre_p9p10p11_ls_avg4_fusegt_1loss.py
@@ -0,0 +1,54 @@
+_base_ = [
+    './gt_extra_small_pre_p9p10p11_nols_avg4_all.py',
+]
+num_classes = 40
+model = dict(
+    type='EncoderDecoder',
+    decode_head=dict(
+    type='SuperformerBottleNeck_ori_mask',
+    classification_feature = 'mask_extralayer',
+    group_pos = ((),(7,8,9,),()), 
+    group_init_strides = (2,2,2,),
+    group_init_kernel_sizes = (2,2,2,),
+    group_layers = {0:256,1:256,2:256},         
+    group_token_init_method = ('avgpool','from_feature','from_feature',),  
+        loss_cls=dict(
+            type='mmdet.CrossEntropyLoss',
+            use_sigmoid=False,
+            loss_weight=1.0,
+            reduction='mean',
+            class_weight=[1.0] * num_classes + [0.1]),
+        loss_mask=dict(
+            type='mmdet.FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            reduction='mean',
+            loss_weight=20.0),
+        loss_dice=dict(
+            type='mmdet.DiceLoss',
+            use_sigmoid=True,
+            activate=True,
+            reduction='mean',
+            naive_dice=True,
+            eps=1.0,
+            loss_weight=1.0),
+        train_cfg=dict(
+            assigner=dict(
+                type='mmdet.HungarianAssigner',
+                match_costs=[
+                    dict(type='mmdet.ClassificationCost', weight=1.0),
+                    dict(
+                        type='mmdet.FocalLossCost',
+                        weight=20.0,
+                        binary_input=True),
+                    dict(
+                        type='mmdet.DiceCost',
+                        weight=1.0,
+                        pred_act=True,
+                        eps=1.0)
+                ]),
+            sampler=dict(type='mmdet.MaskPseudoSampler'))),
+    # training and testing settings
+)
+    
