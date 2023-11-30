887d147a12bebc4b6b0c8b13ac6d559f31676a91
yunfeixie <x908717327@gmail.com>
2023-11-30 01:19:42 +0000

    fixed: flip operation for obj & part

---
+++ configs/superformer/partimagenet/joint/gt_cls/gt_noextra_branch2_small_pre_p9p10p11_nols_avg4_1loss_noungroup_fusegt_objstem_b1.py
@@ -0,0 +1,9 @@
+_base_ = [
+    './gt_noextra_branch_small_pre_p9p10p11_nols_avg4_1loss_noungroup_fusegt.py',
+]
+model = dict(
+    decode_head=dict(
+    obj_stages_pos = (0,1,),
+))
+train_dataloader = dict(
+    batch_size=1)
