e7e652c376faafb8942ebad8b975c5b9562ca24a
yunfeixie <x908717327@gmail.com>
2024-02-12 11:08:00 +0000

    chores: update config

--- configs/superformer/partimagenet/joint/sp_extra_small_pre_nols_avg4_1loss_convstem_joint_sp.py
+++ configs/superformer/partimagenet/joint/sp_extra_small_pre_nols_avg4_1loss_convstem_joint_sp.py
@@ -15,3 +15,10 @@ model = dict(
     use_group_token = False,
     )
 )
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=1000),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(draw= False, type='SegVisualizationHook'))
